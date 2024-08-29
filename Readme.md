9fbf251488c201869f3e13264f62bd2be9dccd59
Mykola <Shekezzz@gmail.com>
2024-08-29 19:47:13 +0300

    hw_17 end

--- Readme.md
+++ Readme.md
@@ -1,1 +1,13 @@
 ## ОПИС
+
+ДЗ 17. Реалізуйте функцію removeElement
+
+Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+
+Наприклад:
+
+Copy code
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5 );
+console.log(array);
+// Результат: [1, 2, 3, 4, 6, 7]
--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,15 @@
 'use strict'
 
+const array = [1, 2, 3, 4, 5, 6, 7 , 8, 9, 10];
+
+const removeElement = function(array, item){
+    let index = array.indexOf(userNumber)
+    if (index > -1){
+        array.splice(index, 1)
+    }
+}
+
+const userNumber = +prompt("Введіть число від 1 до 10, для того щоб його видалить")
+
+removeElement(array, userNumber );
+console.log(array);

0055b60c0bc31dab86eb6162ca2c0327b4fc4e51
Mykola <Shekezzz@gmail.com>
2024-09-01 21:55:07 +0300

    hw_19 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,35 @@
 'use strict'
 
+const mainFunction = function(callback){
+    const userNumber = +prompt("input num");
+    const userDegree = +prompt("input degree");
+
+    callback(userNumber, userDegree);
+};
+
+const exponentiation = function(userNumber, userDegree){
+    const result = Math.pow(userNumber, userDegree);
+    alert(result);
+};
+
+const multiplay = function(userNumber, userDegree){
+    const result = (userNumber * userDegree);
+    alert(result);
+};
+
+const division = function(userNumber, userDegree){
+    const result = (userNumber / userDegree);
+    alert(result);
+};
+
+const modulo = function(userNumber, userDegree){
+    const result = (userNumber % userDegree);
+    alert(result);
+}
+
+
+mainFunction(exponentiation);
+mainFunction(multiplay);
+mainFunction(division);
+mainFunction(modulo);
+

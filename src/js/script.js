ff52dcee1346767d23609e81518e1bf1463e601c
Mykola <Shekezzz@gmail.com>
2024-09-04 19:18:10 +0300

    hw_21 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,20 @@
 'use strict'
 
+const length = +prompt("enter lenght")
+const arr = []
+
+for (let i = 0; i < length; i++){
+    let element = prompt(`enter element ${i + 1}:`)
+    arr.push(element)
+}
+
+
+
+console.log(arr);
+console.log(arr.sort(function(a, b){
+    return a - b;
+}));
+arr.splice(1, 3);
+console.log(arr);
+
+

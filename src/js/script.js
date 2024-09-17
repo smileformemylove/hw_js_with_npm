029098b3601861849e1c18b769bfd329c4230ca3
Mykola <Shekezzz@gmail.com>
2024-09-17 15:09:47 +0300

    hw_24 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,14 @@
 'use strict'
 
+
+for (let i = 0; i < 5; i++){
+    var a = i + 20;
+    let b = i + 20;
+    const c = i + 20;
+    console.log(c)
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
+

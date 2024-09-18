8401ee2ba072004b73e569411dcd0252f5b6b671
Mykola <Shekezzz@gmail.com>
2024-09-18 20:23:47 +0300

    hw_27 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,34 @@
 'use strict'
 
+const users = [
+    {
+        name: "John",
+        age: "18",
+    },
+    {
+        name: "Mark",
+        age: "28",
+    },
+    {
+        name: "Nick",
+        age: "25",
+    },
+    {
+        name: "Sasha",
+        age: "32",
+    },
+    {
+        name: "Steve",
+        age: "18",
+    },
+]
+
+const getUsersInfo = function(){
+    console.log(this.name + ` is ` + this.age + ` years old`)
+}
+
+// реалізуємо bind
+for (const user of users){
+    const bind = getUsersInfo.bind(user , user.name);
+    bind();
+}

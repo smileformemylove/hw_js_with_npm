e5dca1bf4be5c09c5b21b6ffa41afb1a0f85f705
Mykola <Shekezzz@gmail.com>
2024-09-18 19:20:22 +0300

    hw_28 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,31 @@
 'use strict'
 
+//task1
+const getFactorial = (num) => {
+    if (num === 0 || num === 1) {
+        return 1;
+    }
+    return num * getFactorial(num - 1);
+
+}
+console.log(getFactorial(8))
+
+
+//task2
+const getDegree = (num, degree) => {
+    if (degree === 0){
+        return 1
+    }
+    return num * getDegree(num, degree - 1)
+}
+console.log(getDegree(3, 3))
+
+
+//task3
+const getSum = (a , b) => {
+    if (b === 0){
+        return a
+    }
+    return getSum(a + 1, b - 1);
+}
+console.log(getSum(25,25))

8cdc4d49c36231aa7a231e4c69d4c1e62593f20e
Mykola <Shekezzz@gmail.com>
2024-09-20 15:12:28 +0300

    hw_26 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,29 @@
 'use strict'
 
+
+function insertWordInto(initialStr) {
+    let originalWords = initialStr.split(' ');
+    let index = 1;
+
+    return function(word) {
+        let wordsCopy = [...originalWords];
+
+        if (index === 1) {
+            wordsCopy.splice(index, 0, word);
+        } else if (index === 0) {
+            wordsCopy.unshift(word);
+        } else if (index === wordsCopy.length) {
+            wordsCopy.push(word);
+        }
+
+        index = (index + 1) % 3;
+        return wordsCopy.join(' ');
+    };
+}
+
+const insert = insertWordInto('hello world');
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+

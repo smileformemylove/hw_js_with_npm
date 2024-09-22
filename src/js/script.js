4f693939f44e90129d6f7542e1db7fda537574b4
Mykola <Shekezzz@gmail.com>
2024-09-22 19:11:07 +0300

    hw_29

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,28 @@
 'use strict'
 
+const complexArray = [
+    [1, 2, [3.1, 3.2, [3.21, 3.22]], 4],
+    [5, [6, [7, [8, 9]]]],
+    [[10, 11], 12, 13]
+];
+
+const result = [];
+
+const flat = function(arr){
+    if (arguments.length > 1) throw new Error('Function accepts only 1 argument, too much arguments provided');
+
+    if (arr.every(item => !Array.isArray(item))) {
+        return arr.slice();
+    }
+
+    for (const item of arr){
+        if(Array.isArray(item)) flat(item);
+         else result.push(item)
+    }
+    return result;
+
+}
+
+console.log(flat(complexArray));
+console.log(flat([1, 2, 3]));
+

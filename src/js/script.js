ae39ffb7f84a4ac756c3157175ccab5f04140761
Mykola <Shekezzz@gmail.com>
2024-09-24 14:57:35 +0300

    hw_31

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,27 @@
 'use strict'
 
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () { // показывает текущую ступеньку
+        alert(this.step);
+        return this;
+    }
+};
+
+
+ladder.up().up().down().showStep(); // 1
+
+// ladder.up().up().down().up().down().down().down().showStep();
+
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1

dad8ffc125500d374d3868affa9ef1eada052b78
Mykola <Shekezzz@gmail.com>
2024-09-24 14:51:04 +0300

    hw_30

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,15 @@
 'use strict'
 
+const sum = () => {
+    let total = 0;
+    return (num) => {
+        total += num;
+        return total
+    }
+}
+
+const myNum = sum();
+
+console.log(myNum(3));
+console.log(myNum(5));
+console.log(myNum(20));

c5760ca435ac8ee34e04a66d84d556764cadeef0
Mykola <Shekezzz@gmail.com>
2024-09-26 14:14:56 +0300

    hw_33

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,12 @@
 'use strict'
 
+const img = [
+    "1.jpg", "2.jpg", "3.jpg", "4.jpg",
+    "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"
+];
+
+setInterval(function() {
+    const randomIndex = Math.floor(Math.random() * img.length);
+    const randomImage = img[randomIndex];
+    document.getElementById("randomImage").src = `img/${randomImage}`;
+}, 1000);

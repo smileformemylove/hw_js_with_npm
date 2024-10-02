a0491dbd4fdce11e794a05e0eb878cd1aa09c45f
Mykola <Shekezzz@gmail.com>
2024-10-02 20:58:31 +0300

    hw_32

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,24 @@
 'use strict';
 
 let count = 0;
-const table = document.getElementById('myTable').getElementsByTagName('tbody')[0];
 
 const appendHTMLElement = (parentEL = null, elementToAppend = null) => {
     if (!parentEL || !elementToAppend) return;
     parentEL.append(elementToAppend);
 };
 
-const generateTable = () => {
-    const tableSize = 10;
+const generateTable = (rows = 0, columns = 0 ) => {
 
     const table = document.createElement('table');
-    table.className = 'table table-dark table-striped-columns';
+    table.className = 'table table-white table-striped-columns';
     const tbody = document.createElement('tbody');
 
     table.append(tbody);
 
-    for (let i = 0; i < tableSize; i++) {
+    for (let i = 0; i < rows; i++) {
         const tr = document.createElement('tr');
 
-        for (let c = 0; c < tableSize; c++) {
+        for (let c = 0; c < columns; c++) {
             const td = document.createElement('td');
             td.innerHTML = '';
             tr.append(td);
@@ -34,18 +32,15 @@ const generateTable = () => {
 
 
 const tableContainer = document.getElementById('myTable');
-tableContainer.innerHTML = '';
-const newTable = generateTable();
+const newTable = generateTable(10, 10);
 appendHTMLElement(tableContainer, newTable);
-
-const tableBody = newTable.getElementsByTagName('tbody')[0];
 
 setInterval(() => {
     if (count < 100) {
         const rowIndex = Math.floor(count / 10);
         const cellIndex = count % 10;
 
-        const row = tableBody.rows[rowIndex];
+        const row = newTable.rows[rowIndex];
         const cell = row.cells[cellIndex];
         console.log(count)
         cell.textContent = count + 1;

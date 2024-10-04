aa698f04a87953a87b850cb8a56091aece5f041f
Mykola <Shekezzz@gmail.com>
2024-10-04 19:57:30 +0300

    hw_34

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,15 @@
 'use strict'
 
+const hoverDiv = document.getElementById('hoverDiv');
+const shadowDiv = document.getElementById('shadowDiv');
+
+
+hoverDiv.addEventListener('mouseover', () => {
+    shadowDiv.setAttribute('data-visible', 'true');
+});
+
+
+hoverDiv.addEventListener('mouseout', () => {
+    shadowDiv.setAttribute('data-visible', 'false');
+});
+

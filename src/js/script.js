10eefd856c24b118e74e23a08aeb2065e64040bc
Mykola <Shekezzz@gmail.com>
2024-10-07 18:06:52 +0300

    hw_35

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,34 @@
-'use strict'
+'use strict';
+
+const myName = "Kolya";
+const addConsoleLi = document.querySelectorAll('li');
+
+addConsoleLi.forEach((item) => {
+    console.log(item.textContent);
+});
+
+
+const ulAttributes= document.querySelector('ul');
+const ulAttributesArray = [];
+const ulAttributesArrayName = [];
+
+for(let attr of ulAttributes.attributes){
+    ulAttributesArray.push(attr.value);
+    ulAttributesArrayName.push(attr.name)
+}
+
+console.log(ulAttributesArray);
+console.log(ulAttributesArrayName);
+
+
+const lastLi = ulAttributes.lastElementChild;
+lastLi.textContent = `Привіт, мене звати ${myName}`;
+
+const firstLi = ulAttributes.firstElementChild;
+firstLi.setAttribute('data-my-name', myName);
+
+ulAttributes.removeAttribute('data-dog-tail');
+
+
+
 

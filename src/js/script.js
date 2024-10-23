88f6eb39df8f1add4c379e8011b65cb93b644210
Mykola <Shekezzz@gmail.com>
2024-10-23 19:18:56 +0300

    hw 40

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,43 @@
-'use strict'
+'use strict';
+
+
+const user = {
+    name: 'kolya',
+    age: '12',
+
+    get getName(){
+        return this.name;
+    },
+
+    set setName(newName){
+        if(typeof newName === 'string' && newName.length > 0){
+            this.name = newName;
+        }
+        else throw new Error('name is not valid');
+    },
+
+    get getAge(){
+        return this.age;
+    },
+
+    set setAge(newAge){
+        if(typeof newAge === 'number' && newAge > 0 && newAge < 120){
+            this.age = newAge;
+        }
+        else throw new Error('age is not valid');
+    }
+}
+
+console.log(user.getName);
+user.setName = 'vanya';
+console.log(user.getName);
+
+console.log(user.getAge);
+user.setAge = 119;
+console.log(user.getAge);
+
+
+
+
 
 

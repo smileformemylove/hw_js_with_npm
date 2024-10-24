23a4aa684a2ed0791d36ee5b1f7521f9211ed5e2
Mykola <Shekezzz@gmail.com>
2024-10-24 22:29:07 +0300

    hw 39

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,45 @@
-'use strict'
+'use strict';
 
+const deepFreeze = (obj) => {
+    Object.freeze(obj);
+
+    Object.getOwnPropertyNames(obj).forEach((prop) => {
+        const value = obj[prop];
+
+        if (value && typeof value === 'object' && !Object.isFrozen(value)) {
+            deepFreeze(value);
+        }
+    })
+    return obj;
+}
+
+let user = {
+    data: {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: {
+            a1: 1,
+            b1: 2,
+            c1: 3,
+            d1: {
+                a2: 3,
+                b2: 3,
+                c2: 3,
+            }
+        },
+    }
+}
+
+deepFreeze(user);
+
+// console.log(user.data.b)
+// console.log(user.data.d.d1.a2)
+
+user.data.c = 5
+user.data.d.d1.a2 = 5;
+
+
+console.log(user.data.c)
+console.log(user.data.d.d1.a2)
 
